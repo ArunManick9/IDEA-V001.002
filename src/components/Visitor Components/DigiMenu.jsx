9b43cd199a6e694f9aae76262f952aea4a29f8fe
ArunManick9 <arunmanicks.12@gmail.com>
2024-10-31 22:43:26 +0530

    Tweaks for Confimration Screen

--- src/components/Visitor Components/DigiMenu.jsx
+++ src/components/Visitor Components/DigiMenu.jsx
@@ -1,5 +1,5 @@
 // DigiMenu.js
-import React, { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getdetailedmenu } from "../../services/supported_api";
 import { FaShoppingCart, FaChevronDown, FaChevronUp } from "react-icons/fa";
