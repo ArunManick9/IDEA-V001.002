95efebd25e3ed86dc9ef93302ee989b47029981b
ArunManick9 <arunmanicks.12@gmail.com>
2024-11-19 01:07:06 +0530

    changes subscribe live update of orders to agent

--- src/components/Order Management Components/Orders.jsx
+++ src/components/Order Management Components/Orders.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import supabase from '../../services/supabase';
 import { updateOrderStatus } from '../../services/supported_api';
-
+//test
 export default function Orders() {
   const [buckets, setBuckets] = useState({
     newOrders: [],
