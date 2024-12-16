f52029f1b71308563c83e33fc87a36be531c2be1
Magesh J <magesh.lbj23@gmail.com>
2024-12-17 01:58:26 +0530

    half baked slider (not confident)

--- src/components/Visitor Components/HighlightBanner.jsx
+++ src/components/Visitor Components/HighlightBanner.jsx
@@ -5,15 +5,19 @@ import {
 	faCircleArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { progress } from "framer-motion";
 
 const HighlightBanner = ({ highlightDetails }) => {
 	const [items, setItems] = useState([]);
 	const [current, setCurrent] = useState(0);
+	const itemsRef = useRef([]);
 
 	useEffect(() => {
 		setItems(JSON.parse(highlightDetails.associate_item || "[]"));
 	}, [highlightDetails]);
+
+	useEffect(() => {
+		itemsRef.current = itemsRef.current.slice(0, items.length);
+	}, [items]);
 
 	const previousSlide = () => {
 		setCurrent((prev) => (prev + 1) % items.length);
@@ -27,12 +31,19 @@ const HighlightBanner = ({ highlightDetails }) => {
 	const [animationClass, setAnimationClass] = useState("");
 	const ref = useRef(false);
 
-	useEffect(() => {
-		console.log(
-			`current item ${current}`,
-			items.length,
-			`for item ${highlightDetails.id}`
+	const isVisibleInViewport = (element) => {
+		const rect = element?.getBoundingClientRect();
+		if (!rect) return;
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <=
+				(window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 		);
+	};
+
+	useEffect(() => {
 		if (ref.current) {
 			// Trigger animation on re-render
 			setAnimationClass("white-fade");
@@ -48,14 +59,27 @@ const HighlightBanner = ({ highlightDetails }) => {
 
 	return (
 		<div className={`banner ${animationClass}`}>
-			<div className="banner__background">
-				<img
-					className="banner__background--image"
-					src={getCurrentItem().image}
-					alt="background"
-				/>
+			<div className="banner-items">
+				{items.map((item, index) => (
+					<div
+						className={`banner__content ${
+							current === index ? "banner__content--active" : ""
+						}`}
+						key={item.id}
+						ref={(el) => (itemsRef.current[current] = el)}
+					>
+						<div className="banner__background">
+							<div className="banner__name">{item.name}</div>
+							<img
+								className="banner__background--image"
+								src={item.image}
+								alt="background"
+							/>
+						</div>
+					</div>
+				))}
 			</div>
-			<div className="banner__name">{getCurrentItem().name}</div>
+
 			<div className="banner__icons">
 				<FontAwesomeIcon icon={faCircleArrowLeft} onClick={nextSlide} />
 				<FontAwesomeIcon icon={faCircleArrowRight} onClick={previousSlide} />
@@ -64,7 +88,9 @@ const HighlightBanner = ({ highlightDetails }) => {
 				{Array.from({ length: items.length }, (_, i) => i).map((_, index) => (
 					<div
 						className={`banner__dots-unit ${
-							index === current ? "banner__dots-unit--active" : ""
+							index === isVisibleInViewport(itemsRef.current[index])
+								? "banner__dots-unit--active"
+								: ""
 						}`}
 						key={index}
 					></div>
